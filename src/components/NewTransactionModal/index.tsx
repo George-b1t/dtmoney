b7eb8b504d61d4a3b4f4d889c1e15ef656138961
George-b1t <george.soares509@gmail.com>
2021-11-08 17:18:07 -0300

    criando-proprio-hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
-import { FormEvent, useContext, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -15,7 +15,7 @@ interface NewTransactionModalProps {
 };
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [ title, setTitle ] = useState('');
   const [ category, setCategory ] = useState('');
